d443794036340069b9d365052866e1fba1129e72
G.Stepanyan <>
2019-04-03 01:18:41 +0400

    Initial project setup

---
+++ LibForIshkan/BarevApeerJan.swift
@@ -0,0 +1,9 @@
+//
+//  BarevApeerJan.swift
+//  LibForIshkan
+//
+//  Created by Gor Stepanyan on 4/3/19.
+//  Copyright © 2019 Tootak Software. All rights reserved.
+//
+
+import Foundation
---
+++ LibForIshkan/LibForIshkan.h
@@ -0,0 +1,19 @@
+//
+//  LibForIshkan.h
+//  LibForIshkan
+//
+//  Created by Gor Stepanyan on 4/3/19.
+//  Copyright © 2019 Tootak Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibForIshkan.
+FOUNDATION_EXPORT double LibForIshkanVersionNumber;
+
+//! Project version string for LibForIshkan.
+FOUNDATION_EXPORT const unsigned char LibForIshkanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibForIshkan/PublicHeader.h>
+
+
